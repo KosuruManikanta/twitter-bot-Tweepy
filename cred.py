a9ce4cd4780440cc64691fb772025e7fa593050b
KosuruManikanta <68510326+KosuruManikanta@users.noreply.github.com>
2020-10-03 17:09:03 +0530

    second commit

--- cred.py
+++ cred.py
@@ -1,8 +1,9 @@
 import tweepy
 
-#auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-#auth.set_access_token(access_token, access_token_secret)
-#api = tweepy.API(auth)
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
 
 
 #replace consumer_key,consumer_secret,access_token,access_token_secret 
--- twitter trends.py
+++ twitter trends.py
@@ -11,3 +11,7 @@ print("The top trends for the location are :")
 for value in trends:
     for trend in value['trends']:
         print(trend['name'])
+
+
+#woeid woeid is a unique code set for locations search for woeid code of the location you want in google
+#here woeid code 2295414 belongs to hyderabad India
