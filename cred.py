a3917cc472a2bd57fb893348dce1722a067e2bd3
KosuruManikanta <68510326+KosuruManikanta@users.noreply.github.com>
2020-10-03 17:22:41 +0530

    third commit

--- cred.py
+++ cred.py
@@ -1,9 +1,15 @@
 import tweepy
 
+auth = tweepy.OAuthHandler("yFTkg0Adh2cLntJb5WcejQZGn", "9pjykBIAXQaZXiFydtjbVdk1M4EpQlWfRrNKNkZz0bVGKjWtrs"
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+)
+auth.set_access_token("592434187-acUdZrwv4Mc0SSWJF0UP6g5qEADPjYPkoHH9pLqh", "EiCkbKgpKUQHxl41ZT1ObqM1jnhQDBvWGm6x3ah3pgmNd")
+
 api = tweepy.API(auth)
+
+#auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+#auth.set_access_token(access_token, access_token_secret)
+#api = tweepy.API(auth)
 
 
 #replace consumer_key,consumer_secret,access_token,access_token_secret 
---
+++ teewt.py
@@ -0,0 +1,4 @@
+from cred import *
+
+d= api.update_status('Check out my GIT Repository https://github.com/KosuruManikanta/twitter-bot-tweepy to build twitter bot #twitterbot #TWEEPY #Python 3 this is tweeted from tweepy')
+print(d)
